572419eb0ea14fa1277146b118c4860aac74bf76
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-01-10 16:18:01 -0500

    figured babel-eslint was problem. removed temporarily. will add later. liniting is not priority now

---
+++ client/src/data.js
@@ -0,0 +1,19 @@
+//data.js
+const data = [
+  {
+    _id: 1,
+    author: "Unnit",
+    text: "Wow! This is working!",
+    updateAt: new Date(),
+    createdAt: new Date()
+  },
+  {
+    _id: 2,
+    author: "Chirag",
+    text: "Awesome App!",
+    updateAt: new Date(),
+    createdAt: new Date()
+  }
+];
+
+export default data;
